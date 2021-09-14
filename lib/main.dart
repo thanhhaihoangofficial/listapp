37df706b201e5704f5d1bbd29993243c3894df64
mitsuhacreators <dauphu408@gmail.com>
2021-09-14 14:13:08 +0700

    Hello

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({ Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello DH19CT-LT-AN!'),
+          ),
+      ),
+    );
+  }
+}
